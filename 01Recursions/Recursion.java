777e1a0fe1c58dcb0edc8e2437f04e0fc31a71b6
Charles-H <cweng@stuy.edu>
2017-02-07 22:09:33 -0500

    Finished (hopefully)

--- 01Recursions/Recursion.java
+++ 01Recursions/Recursion.java
@@ -1,34 +1,57 @@
-public class Recursion{ 
+public class Recursion{
+    //required name function
     public static String name(){
 	return "Weng,Charles";
     }
 
-    public static double sqrt throws IllegalArgumentException(double n){
-	if(n < 0){
+    //starts off the recursion and checks if the number given is valid
+    public static double sqrt(double n) throws IllegalArgumentException{
+	if(n < 0)
 	    throw new IllegalArgumentException("sqrt: " + n);
-	}
-	double guess = 1;
-	while (!close(n,guess)){
-	    guess = ((n / guess) + guess) / 2;
-	}
-	
+	return helper(n, 1);
     }
 
+    //this is the function that loops itself until it's good enough
+    public static double helper(double n, double guess){
+	if (close(n, guess * guess)){
+	    return guess;
+	}
+	return helper(n, ((n / guess) + guess) / 2);	
+    }
+
+    //checks if two floats are close (a certain % of each other either way)
     public static boolean close(double a, double b){
 	return close2(a,b) || close2(b,a);
     }
-    
+
+    //absolute value function
     public static double abs(double x){
 	if (x < 0)
 	    return -1 * x;
 	return x;
     }
 
+    //checks if b is within a certain % of a
     public static boolean close2(double a, double b){
 	double x = abs(a * .00000000000001);
-	return a + b > b && a -b < b;
+	return a + x > b && a - x < b;
+    }
+
+    //2 print funtions for debugging
+    public static void p(String n){
+	System.out.println(n);
+    }
+    public static void p(double n){
+	p("" +n);
     }
     
-    public static void main(){
+    public static void main(String[] args){
+	p(name());
+	p(sqrt(25));
+	p(sqrt(2.2));
+	p(sqrt(100.123124513524352));
+	p(sqrt(625 + 123819));
+	p(sqrt(-25));
+	
     }
 }
