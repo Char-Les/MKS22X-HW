269089f262f6bb67d4547c8ba37a3b5323db49e8
Charles Weng School <cweng@stuy.edu>
2017-02-14 12:37:32 -0500

    initial file

---
+++ 03Tour/KnightBoard.java
@@ -0,0 +1,24 @@
+public class KnightBoard{
+    int[][] board;
+    
+    public KnightBoard(int r,int c){
+	board = new int[r][c];
+    }
+    
+    public void solve(){
+	
+    }
+    
+    private boolean solveH(int r, int c, int count){
+	
+    }
+
+    //2 rows; row 1 -> row coord; row 2 -> col coord
+    private int[][] spotsR(int r, int c){
+	
+    }
+    
+    //blank if you never called solve or when there is no solution
+    public String toString(){
+    }
+}
