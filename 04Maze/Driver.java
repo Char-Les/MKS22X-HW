7a46f1edcbeadaf44d8a008ebedb3ab358790b11
Charles-H <cweng@stuy.edu>
2017-03-05 22:11:13 -0500

    complete?

--- 04Maze/Driver.java
+++ 04Maze/Driver.java
@@ -11,14 +11,22 @@ public class Driver{
     }
     
     
-    public static void main(String[]args) throws FileNotFoundException{
+    public static void main(String[]args) throws FileNotFoundException, Exception{
 	File x = new File("data1.dat");
+	p(args.length);
+	if (args.length > 0){
+	    x = new File(args[0]);
+	}
 	Scanner lines = new Scanner(x);
 	while (lines.hasNext()){
 	    p(lines.nextLine());
 	}
 
 	Maze a = new Maze("data1.dat");
+	if (args.length > 0){
+	    a = new Maze(args[0]);
+	}
 	a.setAnimate(true);
+	p(a.solve() + "");
     }
 }
