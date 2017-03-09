df61574cc6c9c21b93581df97e307d3a649cc2ca
Charles Weng School <cweng@stuy.edu>
2017-03-09 12:33:38 -0500

    mm

--- 05USACO/Lake.java
+++ 05USACO/Lake.java
@@ -1,10 +1,10 @@
 import java.util.*;
 import java.io.*;
 public class Lake{
-    private int[][]land;
+    private int[][] land;
     private int waterlv;
     private int[][] lake;
-    private int[][]instructions;
+    private int[][] instructions;
 
     public Lake(String filename) throws FileNotFoundException{
 	File text= new File(filename);
@@ -37,19 +37,63 @@ public class Lake{
 	}
     }
 
+
+
+
+
+
+
+
     public int solve(){
 	for (int x = 0; x < instructions.length; x ++){
-	    stomp(instructions[x][0], instructions[x][1], instructions[x][2]);
+	    stomp(instructions[x][0] - 1, instructions[x][1] - 1, instructions[x][2]);
 	}
 	updateLake();
-	return 0;
+	return sumLake() * 72 * 72;
     }
     
-    public void stomp (int r, int c, int d){
-	//int level = findHigh(r,c) - d;
-	
+    private void stomp (int r, int c, int d){
+	int level = findHigh(r,c) - d;
+	for(int i = 0; i < 9; i ++){
+	    if ( level < land[r + i/3][c + i%3]) land[r + i/3][c + i%3] = level;
+	}
     }
     
+    private int findHigh(int r, int c){
+	int x = land[r][c];
+	for(int i = 1; i < 9; i ++){
+	    if (x < land[r + i/3][c + i%3]) x = land[r + i/3][c + i%3];
+	}
+	return x;
+    }
+
+    private void updateLake(){
+	for     (int r = 0; r < lake.length; r ++){
+	    for (int c = 0; c < lake[r].length; c ++){
+		int level = waterlv - land[r][c];
+		if (level < 0) level = 0;
+		lake[r][c] = level;
+	    }
+	}
+    }
+    
+    private int sumLake(){
+	int ans = 0;
+	for     (int r = 0; r < lake.length; r ++){
+	    for (int c = 0; c < lake[r].length; c ++){
+		ans += lake[r][c];
+	    }
+	}
+	return ans;
+    }
+
+
+
+
+    
+
+
+
     public String stringify(int[][] x){
 	String ans = "";
 	for     (int r = 0; r < x.length; r ++){
@@ -61,23 +105,27 @@ public class Lake{
 	return ans;
     }
     
-    public void updateLake(){
-	for     (int r = 0; r < lake.length; r ++){
-	    for (int c = 0; c < lake[r].length; c ++){
-		int level = waterlv - land[r][c];
-		if (level < 0) level = 0;
-		lake[r][c] = level;
-	    }
-	}
-    }
+
+
+
+
+
+
+
+
+
+
+
     
     public String toString(){
 	//prints pasture and the instructions 
-	return stringify(land) + "\n" + stringify(instructions);
+	return stringify(land) + "\n" + stringify(lake);
     }
     
     public static void main(String[]args)throws FileNotFoundException{
 	Lake test=new Lake("data.txt");
+	System.out.println(test.toString());
+	System.out.println(test.solve());
 	System.out.println(test.toString());
     }
 		
---
+++ 05USACO/USACO.java
@@ -0,0 +1,13 @@
+public class USACO{
+    USACO(){
+	
+    }
+    
+    public int bronze(String filename){
+	Lake x = new Lake(filename);
+	return x.solve();
+    }
+
+
+
+}
