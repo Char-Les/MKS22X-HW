6e90b60aad09a4a1eb0859e734e83ed4617dc7d1
Charles Weng School <cweng@stuy.edu>
2017-03-12 21:43:36 -0400

    progress

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -1,13 +1,31 @@
+import java.io.*;
+
 public class USACO{
     USACO(){
 	
     }
     
     public int bronze(String filename){
-	Lake x = new Lake(filename);
-	return x.solve();
+	try{
+	    Lake x = new Lake(filename);
+	    return x.solve();
+	}catch (FileNotFoundException e){
+	    System.out.println("error");
+	}
+	return -1;
     }
 
-
+    public static void main(String[] args){
+	USACO x = new USACO();
+	if (args.length > 1){
+	    System.out.println("error");
+	    if (args[0].equals("0")){
+		System.out.println(x.bronze(args[1]));
+	    }else if (args[0].equals("1")){
+		//System.out.println(x.silver(args[1]));
+	    }
+	    
+	}
+    }
 
 }
