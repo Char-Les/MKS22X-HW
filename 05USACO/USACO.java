e0a2adfffb084ba6d2d293977909461198096b1c
Charles-H <cweng@stuy.edu>
2017-03-12 22:07:52 -0400

    editing

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -1,31 +1,46 @@
 import java.io.*;
+import java.util.*;
+
 
 public class USACO{
     USACO(){
-	
     }
     
     public int bronze(String filename){
 	try{
 	    Lake x = new Lake(filename);
 	    return x.solve();
-	}catch (FileNotFoundException e){
-	    System.out.println("error");
+	}catch(FileNotFoundException e){
+	    System.exit(1);
 	}
 	return -1;
     }
 
     public static void main(String[] args){
 	USACO x = new USACO();
-	if (args.length > 1){
-	    System.out.println("error");
-	    if (args[0].equals("0")){
-		System.out.println(x.bronze(args[1]));
-	    }else if (args[0].equals("1")){
-		//System.out.println(x.silver(args[1]));
+	try{
+	    //brozne test cases
+	    for(int i = 1; i < 10;i ++ ){
+		int a = x.bronze("data" + i + ".txt");
+		File z = new File("ans" + i + ".txt");
+		Scanner y = new Scanner(z);
+		int b = y.nextInt();
+		System.out.println(i + "   "+ a + "  " + b + "  " + (a == b));
 	    }
+	
 	    
-	}
-    }
+	    // silver
+	    System.out.println("\n\n\n");
 
+	    for(int i = 1; i < 10;i ++ ){
+		int a = x.bronze("data" + i + ".dat");
+		File z = new File("ans" + i + ".dat");
+		Scanner y = new Scanner(z);
+		int b = y.nextInt();
+		System.out.println(i + "   "+ a + "  " + b + "  " + (a == b));
+	    }
+	}catch(FileNotFoundException e){
+	}
+	
+    }
 }
