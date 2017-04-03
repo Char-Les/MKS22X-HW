643595618e79e4db27c207e5a4520d56042848c7
Charles Weng School <cweng@stuy.edu>
2017-04-03 12:32:09 -0400

    tgsd

--- 09MyLinkedList/MyLinkedList.java
+++ 09MyLinkedList/MyLinkedList.java
@@ -4,33 +4,86 @@ public class MyLinkedList{
     
     private class LNode{
 	int value;
-	LNode next;
+	LNode head, tail;
 
-	public LNode(int x, LNode thing){
+	public LNode(int x, LNode next, LNode pre){
 	    value = x;
-	    next = thing
+	    head = next;
+	    tail = pre;
 	}
 
 	public LNode(int x){
 	    value = x;
-	    next = null;
+	    head = null;
+	    tail = null
 	}
     }
 
     public MyLinkedList(){
 	start = null;
+	end = null;
 	size = 0;
     }
     
 
+    
+    //extenstion of the add(Node)
     public boolean add(int x){
-	end.next = x;
+	return add(new Node(x));
     }
 
+    //add a value x to the end;
+    public boolean add(Node x){	
+	//empty list case
+	if (size == 0){
+	    start = x;
+	    end = x;
+	    size ++;
+	    return true;
+	}
+
+	//general case	
+	end.head = x;
+	x.tail = end;
+	end = x;
+	return true;
+    }
+
+    //what does this do??
     public int size(){
 	return size;
+    }//wow so coooooll
+
+
+    //removes a certain node given that node 
+    private void remove(Node x){
+	//x is start and end
+	if (x.tail == null && x.head == null){
+	    start = null;
+	    end = null;
+	    return;
+	}
+	//x is the start
+	if (x.tail == null){
+	    x.head.tail = null;
+	    start = x.head;
+	    return;
+	}
+	//x is the end
+	if (x.head == null){
+	    x.tail.head = null;
+	    end = x.tail;
+	    return;
+	}
+	//x is in the middle
+	x.tail.head = x.head;
+	x.head.tail = x.tail;
     }
+    
 
 
-
+    public String toString(){
+	
+    }
+    
 }
