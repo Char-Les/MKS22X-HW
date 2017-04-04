7b1925afe8eff105c98aa4b5e5257a394ad56f06
Charles-H <cweng@stuy.edu>
2017-04-04 05:12:07 -0400

    thingsdfs

--- 09MyLinkedList/MyLinkedList.java
+++ 09MyLinkedList/MyLinkedList.java
@@ -32,7 +32,7 @@ public class MyLinkedList{
 	return add(new Node(x));
     }
 
-    //add a value x to the end;
+    //add a node to the end;
     public boolean add(Node x){	
 	//empty list case
 	if (size == 0){
@@ -49,12 +49,45 @@ public class MyLinkedList{
 	return true;
     }
 
+    //add after the index you give
+    //0 <= index < size
+    public boolean add(int index, int value){
+	Node x = getNode(x);
+	return add(x, value);
+    }
+
+    //general base add function
+    private boolean add(Node x, int value){
+	Node a = new Node(value);
+	if (x == end){
+	    x.head = a;
+	    end = x;
+	    return true;;
+	}
+
+	x.head.tail = a;
+	a.head = x.head;
+	x.head = a;
+	a.tail = x;
+	return true;	   
+    }
+    
     //what does this do??
     public int size(){
 	return size;
     }//wow so coooooll
 
+    //gets to a certain node index
+    private node getNode(int index){
+	//this is private so the case where index is greater than size is the program's fault
+	Node ans = start;
+	for(;index > 0; index --){
+	    ans = ans.head;
+	}
+	return ans;
+    }
 
+    
     //removes a certain node given that node 
     private void remove(Node x){
 	//x is start and end
