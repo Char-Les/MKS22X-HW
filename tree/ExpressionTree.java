90c683ca0d059e6c500c19005c196f66dc897684
Charles Weng School <cweng@stuy.edu>
2017-04-27 12:24:27 -0400

    ran the test on my code

--- tree/ExpressionTree.java
+++ tree/ExpressionTree.java
@@ -1,26 +1,95 @@
 public class ExpressionTree{
-    /*instance variables, constructors, and some methods not shown*/
+    private char op;
+    private double value;
+    private ExpressionTree left,right;
   
-    /*accessor method for the value, precondition is that isValue() is true.*/
-    private double getValue(){    /*implementation not shown*/ }
-    /*accessor method for the operation, precondition is that isOp() is true.*/
-    private char getOp(){    /*implementation not shown*/  }    
+    /*TreeNodes are immutable, so no issues with linking them across multiple
+     *  expressions. The can be constructed with a value, or operator and 2
+     * sub-ExpressionTrees*/
+    public ExpressionTree(double value){
+	this.value = value;
+	op = '~';
+    }
+    public ExpressionTree(char op,ExpressionTree l, ExpressionTree r){
+	this.op = op;
+	left = l;
+	right = r;
+    }
+  
+  
+  
+    public char getOp(){
+	return op;
+    }
+  
+    /* accessor method for Value, precondition is that isValue() is true.*/
+    private double getValue(){
+	return value;
+    }
     /* accessor method for left, precondition is that isOp() is true.*/
-    private ExpressionTree getLeft(){    /*implementation not shown*/  }
+    private ExpressionTree getLeft(){
+	return left;
+    }
     /* accessor method for right, precondition is that isOp() is true.*/
-    private ExpressionTree getRight(){    /*implementation not shown*/  }
+    private ExpressionTree getRight(){
+	return right;
+    }
+  
+    private boolean isOp(){
+	return hasChildren();
+    }
+    private boolean isValue(){
+	return !hasChildren();
+    }
+  
+    private boolean hasChildren(){
+	return left != null && right != null;
+    }
+  
+  
+    public static void main(String[] args){
+	//ugly main sorry!
+	ExpressionTree a = new ExpressionTree(4.0);
+	ExpressionTree b = new ExpressionTree(2.0);
 
-    private boolean isOp(){    /*implementation not shown*/  }
-    private boolean isValue(){    /*implementation not shown*/  }
+	ExpressionTree c = new ExpressionTree('+',a,b);
+	System.out.println(c);
+	System.out.println(c.toStringPostfix());
+	System.out.println(c.toStringPrefix());
+	System.out.println(c.evaluate());
 
+	ExpressionTree d = new ExpressionTree('*',c,new ExpressionTree(3.5));
+	System.out.println(d);
+	System.out.println(d.toStringPostfix());
+	System.out.println(d.toStringPrefix());
+	System.out.println(d.evaluate());
 
-    /* you write these four methods, hint: think recursively */
+	ExpressionTree ex = new ExpressionTree('-',d,new ExpressionTree(1.0));
+	System.out.println(ex);
+	System.out.println(ex.toStringPostfix());
+	System.out.println(ex.toStringPrefix());
+	System.out.println(ex.evaluate());
+
+	ex = new ExpressionTree('+',new ExpressionTree(1.0),ex);
+	System.out.println(ex);
+	System.out.println(ex.toStringPostfix());
+	System.out.println(ex.toStringPrefix());
+	System.out.println(ex.evaluate());
+
+	ex = new ExpressionTree('/',ex,new ExpressionTree(2.0));
+	System.out.println(ex);
+	System.out.println(ex.toStringPostfix());
+	System.out.println(ex.toStringPrefix());
+	System.out.println(ex.evaluate());
+    
+    
+    }
 
     /*return the expression as an infix notation string with parenthesis*/
     /* The sample tree at the top would be: "( 3 + (2 * 10))"     */
     public String toString(){
 	if(isOp())
-	    return "(" + getLeft().toString() + " " + getOp() + " " + getRight.toString() + ")";
+	    return "(" + getLeft().toString() + " " + getOp() + " " + getRight().toString() + ")";
 	return "" + getValue();
     }
 
@@ -28,7 +97,7 @@ public class ExpressionTree{
     /* The sample tree would be: "3 2 10 * +"     */
     public String toStringPostfix(){
 	if(isOp())
-	    return getLeft().toString() + " " + getRight.toString() + " " + getOp();
+	    return getLeft().toString() + " " + getRight().toString() + " " + getOp();
 	return "" + getValue();
     }
 
@@ -36,7 +105,7 @@ public class ExpressionTree{
     /* The sample tree would be: "+ 3 * 2 10"     */
     public String toStringPrefix(){
 	if(isOp())
-	    return getOp() + " " + getLeft().toString() + " " + getRight.toString();
+	    return getOp() + " " + getLeft().toString() + " " + getRight().toString();
 	return "" + getValue();
     }
 
@@ -45,7 +114,7 @@ public class ExpressionTree{
 	if(isOp()){
 	    double a = getRight().evaluate();
 	    double b = getLeft().evaluate();
-	    switch (x){
+	    switch (getOp()){
 	    case '+':
 		return b + a;
 		
@@ -58,11 +127,11 @@ public class ExpressionTree{
 	    case '/':
 		return b / a;
 		
-	    case "%":
+	    case '%':
 		return b % a;
 	    }
-	  
-	    return getValue();
 	}
+	return getValue();
 
     }
+}
