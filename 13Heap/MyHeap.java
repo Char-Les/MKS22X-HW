6ef6bf5a71ee8effc11f6239756d981c7cffb36c
Charles Weng School <cweng@stuy.edu>
2017-05-03 12:28:52 -0400

    start of heap and clean up

---
+++ 13Heap/MyHeap.java
@@ -0,0 +1,9 @@
+public class Heap{
+
+    public Heap(){}
+    public Heap(){}
+    public void add(){}
+    public String remove(){}
+    public String peak(){}
+
+}
---
+++ Heap.java
@@ -0,0 +1,10 @@
+public class MyHeap{
+    
+    
+    public Heap(){}
+    public Heap(){}
+    public void add(String x){}
+    public String remove(){}
+    public String peak(){}
+
+}
