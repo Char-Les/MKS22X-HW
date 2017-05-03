3cf462f3aaf9037fdff14ed4f69655b80ee9c168
Charles Weng School <cweng@stuy.edu>
2017-05-03 12:33:26 -0400

    sonethuig

--- 13Heap/MyHeap.java
+++ 13Heap/MyHeap.java
@@ -1,8 +1,10 @@
-public class Heap{
-
+public class MyHeap{
+    ArrayList<String> heap;
+    boolean max;
+    
+    public Heap(boolean a){}
     public Heap(){}
-    public Heap(){}
-    public void add(){}
+    public void add(String x){}
     public String remove(){}
     public String peak(){}
 
